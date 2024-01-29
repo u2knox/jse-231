3faa6833fe8afbd0926afa1d16146eca5931f649
u2knox <ttuleubayev@alseco.kz>
2024-01-29 21:50:32 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div v-for="" class="card">
+      <h3>{{ item.name }}</h3>
+      <h5>{{ item.price }}</h5>
+      <div>
+        <button>Купить</button>
+        <div>
+          <button>-</button>
+          <span>Количество</span>
+          <button>+</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const color = ref('')
+const arr = ref([
+  {
+    title: 'Зеленый',
+    value: 'teal'
+  },
+  {
+    title: 'Красный',
+    value: 'red'
+  },
+  {
+    title: 'Синий',
+    value: 'blue'
+  }
+])
+</script>
+
+<style scoped>
+.first {
+  color: red;
+}
+.second {
+  color: teal;
+}
+</style>
